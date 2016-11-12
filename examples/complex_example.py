dc3ab935021e5e0b71347705af70a5a95bbbe561
Dan Taylor <qubitron@users.noreply.github.com>
2016-11-12 12:32:41 -0800

    Include mutation example in complex_example.py
    This does not currently work but thought I would give my best shot at adding an example of passing an input object with a mutation. Would be great if someone could update this to a working example.
    
    When I try to run this I get:
    
    Traceback (most recent call last):
      File "complex_example.py", line 31, in <module>
        class Mutation(graphene.ObjectType):
      File "complex_example.py", line 32, in Mutation
        setAddress = SetAddress.Field()
      File "/usr/local/lib/python3.5/site-packages/graphene/types/field.py", line 53, in __init__
        self.args = to_arguments(args or OrderedDict(), extra_args)
      File "/usr/local/lib/python3.5/site-packages/graphene/types/argument.py", line 40, in to_arguments
        raise ValueError('Unknown argument "{}".'.format(default_name))
    ValueError: Unknown argument "geo".

--- examples/complex_example.py
+++ examples/complex_example.py
@@ -1,6 +1,6 @@
 import graphene
 
-
+# Query example
 class GeoInput(graphene.InputObjectType):
     lat = graphene.Float(required=True)
     lng = graphene.Float(required=True)
@@ -17,8 +17,21 @@ class Query(graphene.ObjectType):
         geo = args.get('geo')
         return Address(latlng="({},{})".format(geo.get('lat'), geo.get('lng')))
 
+# Mutate example
+class SetAddress(graphene.Mutation):
+    class Input:
+        geo = graphene.InputField(GeoInput)
 
-schema = graphene.Schema(query=Query)
+    latlng = graphene.Field(Address) 
+    def mutate(self, args, context, info):
+        print("Hello")
+        geo = args.get('geo')
+        return SetAddress(latlng="({},{})".format(geo.get('lat'), geo.get('lng')))
+
+class Mutation(graphene.ObjectType):
+    setAddress = SetAddress.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 query = '''
     query something{
       address(geo: {lat:32.2, lng:12}) {
@@ -27,6 +40,13 @@ query = '''
     }
 '''
 
+mutation = '''
+    mutatation somethingelse{ 
+      setAddress(geo: {lat:32.2, lng:12}) {
+        latlng
+      }
+    }
+'''
 
 def test_query():
     result = schema.execute(query)
@@ -37,7 +57,17 @@ def test_query():
         }
     }
 
+    result = schema.execute(mutation)
+    assert not result.errors
+    assert result.data == {
+        'address': {
+            'latlng': "(32.2,12.0)",
+        }
+    }
 
 if __name__ == '__main__':
     result = schema.execute(query)
+    print(result.data['address']['latlng'])
+
+    result = schema.execute(mutation)
     print(result.data['address']['latlng'])
